bb403da1e649d02d6698441311d20b39b3e08803
anirudh <anirudh@calvisitor-10-105-142-143.calvisitor.1918.berkeley.edu>
2016-11-12 05:11:01 -0800

    Initial Commit

---
+++ mello/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mello
+//
+//  Created by Anirudh on 12/11/16.
+//  Copyright © 2016 Cal Hacks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
